744da02dbb4058c3037daf6c3227db0d4c53b821
HelplessSoldier <patrick1234143211@gmail.com>
2023-09-11 14:59:34 -0700

    refactored caesar cipher

--- ceasarCipher/ceasarCipher.js
+++ ceasarCipher/ceasarCipher.js
@@ -1,39 +1,26 @@
+function _shiftChars(charcodes, range, shiftFactor) {
+  for (let i = 0; i < charcodes.length; i++) {
+    if (charcodes[i] >= range[0] && charcodes[i] <= range[1]) {
+      charcodes[i] += shiftFactor;
+      // overshot, need to wrap
+      if (charcodes[i] > range[1]) {
+        charcodes[i] = range[0] + (charcodes[i] - range[1]) - 1;
+      }
+    }
+  }
+}
+
 function ceasarCipher(string, shiftFactor) {
   const lowercaseRange = [97, 122];
   const uppercaseRange = [65, 90];
 
-  // get ord of each char
   let charcodes = [];
   for (let char of string) {
     charcodes.push(char.charCodeAt(0));
   }
 
-  for (let i = 0; i < charcodes.length; i++) {
-    // lowercase charcodes:
-    if (
-      charcodes[i] >= lowercaseRange[0] &&
-      charcodes[i] <= lowercaseRange[1]
-    ) {
-      charcodes[i] += shiftFactor;
-      // overshot, need to wrap
-      if (charcodes[i] > lowercaseRange[1]) {
-        charcodes[i] =
-          lowercaseRange[0] + (charcodes[i] - lowercaseRange[1]) - 1;
-      }
-    }
-    // uppercase charcodes:
-    if (
-      charcodes[i] >= uppercaseRange[0] &&
-      charcodes[i] <= uppercaseRange[1]
-    ) {
-      charcodes[i] += shiftFactor;
-      // overshot, need to wrap
-      if (charcodes[i] > uppercaseRange[1]) {
-        charcodes[i] =
-          uppercaseRange[0] + (charcodes[i] - uppercaseRange[1]) - 1;
-      }
-    }
-  }
+  _shiftChars(charcodes, lowercaseRange, shiftFactor);
+  _shiftChars(charcodes, uppercaseRange, shiftFactor);
 
   let res = "";
   res += String.fromCharCode(...charcodes);
